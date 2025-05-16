29c8cf8d33186f996c2f3d78309d7dbf52e3ef56
Jocelyn FOUCHER <jfoucher@f4r1s3.clusters.42paris.fr>
2025-05-16 14:45:45 +0200

    hourly weather scrollview implemented

--- mobileModule03/advanced_weather_app/lib/widgets/weather_tabs/today_weather_tab.dart
+++ mobileModule03/advanced_weather_app/lib/widgets/weather_tabs/today_weather_tab.dart
@@ -1,4 +1,5 @@
 import 'package:advanced_weather_app/models/hourly_weather_model.dart';
+import 'package:advanced_weather_app/utils/weather_icon_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -35,46 +36,84 @@ class TodayWeatherTab extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               Text(
-                '${weatherModel.location.region}',
+                '${weatherModel.location.region}, ${weatherModel.location.country}',
                 style: Theme.of(context).textTheme.titleMedium,
               ),
-              Text('${weatherModel.location.country}'),
               const SizedBox(height: 16.0),
-              ...weatherModel.hourlyWeatherList.map((hourly) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        '${hourly.time.hour}:${hourly.time.minute}',
-                        softWrap: true,
+              const SizedBox(height: 320.0),
+              
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: weatherModel.hourlyWeatherList.map<Widget>((hourly) {
+                    final time = '${hourly.time.hour.toString().padLeft(2, '0')}:${hourly.time.minute.toString().padLeft(2, '0')}';
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: HourWeatherInfo(
+                        time: time,
+                        weatherDescription: hourly.weatherDescription,
+                        temperature: hourly.temperature.toString(),
+                        windSpeed: hourly.windSpeed.toString(),
                       ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${hourly.temperature}°C',
-                        softWrap: true,
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${hourly.weatherDescription}',
-                        softWrap: true,
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '${hourly.windSpeed} km/h',
-                        softWrap: true,
-                      ),
-                    ),
-                  ],
-                );
-              }),
+                    );
+                  }).toList(),
+                ),
+              ),
             ],
           ),
         );
       },
+    );
+  }
+}
+
+class HourWeatherInfo extends StatelessWidget {
+  final String time;
+  final String weatherDescription;
+  final String temperature;
+  final String windSpeed;
+  
+  const HourWeatherInfo({
+    super.key,
+    this.time = '00:00',
+    this.weatherDescription = 'Clear sky',
+    this.temperature = '28.0',
+    this.windSpeed = '13.4',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          time,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(height: 16.0),
+        Icon(
+            getWeatherIcon(weatherDescription),
+            size: 36.0,
+            color: const Color(0xFF7E57C2),
+          ),
+          const SizedBox(height: 8.0),
+          // Temperature
+        Text(
+          '${temperature}°C',
+          style: TextStyle(fontSize: 18.0, color: Colors.orange),
+        ),
+        // Wind speed
+        Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.air, size: 16.0, color: Colors.blue),
+          const SizedBox(width: 8.0),
+          Text(
+            '${windSpeed} km/h',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ],
+      ),
+      ],
     );
   }
 }
